3edb8a589e7bec19060a51d4c3fcfd66029c9cd1
Nikolay <75427338+korhenon@users.noreply.github.com>
2025-05-21 16:34:14 +0300

    Add custom theme

--- app/src/main/java/com/example/flights/ui/theme/Theme.kt
+++ app/src/main/java/com/example/flights/ui/theme/Theme.kt
@@ -1,58 +1,87 @@
 package com.example.flights.ui.theme
 
-import android.app.Activity
-import android.os.Build
-import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
-import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.platform.LocalContext
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.Immutable
+import androidx.compose.runtime.staticCompositionLocalOf
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
 
-private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+@Immutable
+data class FlightsColors(
+    val primary: Color,
+    val background: Color,
+    val error: Color,
+    val warning: Color,
+    val success: Color,
+    val onBackground: Color
 )
 
-private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+@Immutable
+data class FlightsTypography(
+    val textXs: TextStyle,
+    val textSm: TextStyle,
+    val textMd: TextStyle,
+    val textLg: TextStyle,
+    val textXl: TextStyle,
+    val displayXs: TextStyle,
+    val displaySm: TextStyle,
+    val displayMd: TextStyle,
+    val displayLg: TextStyle,
+    val displayXl: TextStyle,
+    val display2xl: TextStyle
 )
+
+val LocalFlightsColors = staticCompositionLocalOf {
+    FlightsColors(
+        primary = Color.Unspecified,
+        background = Color.Unspecified,
+        error = Color.Unspecified,
+        warning = Color.Unspecified,
+        success = Color.Unspecified,
+        onBackground = Color.Unspecified
+    )
+}
+
+val LocalFlightsTypography = staticCompositionLocalOf {
+    FlightsTypography(
+        textXs = TextStyle.Default,
+        textSm = TextStyle.Default,
+        textMd = TextStyle.Default,
+        textLg = TextStyle.Default,
+        textXl = TextStyle.Default,
+        displayXs = TextStyle.Default,
+        displaySm = TextStyle.Default,
+        displayMd = TextStyle.Default,
+        displayLg = TextStyle.Default,
+        displayXl = TextStyle.Default,
+        display2xl = TextStyle.Default
+    )
+}
 
 @Composable
-fun FlightsTheme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
-    content: @Composable () -> Unit
-) {
-    val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-        }
-
-        darkTheme -> DarkColorScheme
-        else -> LightColorScheme
-    }
-
-    MaterialTheme(
-        colorScheme = colorScheme,
-        typography = Typography,
+fun FlightsTheme(content: @Composable () -> Unit) {
+    val colors = FlightsColors(
+        primary = Primary,
+        background = Color.White,
+        error = Error,
+        warning = Warning,
+        success = Success,
+        onBackground = Gray800
+    )
+    val typography = createFlightsTypography(colors.onBackground)
+    CompositionLocalProvider(
+        LocalFlightsColors provides colors,
+        LocalFlightsTypography provides typography,
         content = content
     )
+}
+
+object FlightsTheme {
+    val colors: FlightsColors
+        @Composable
+        get() = LocalFlightsColors.current
+    val typography: FlightsTypography
+        @Composable
+        get() = LocalFlightsTypography.current
 }
--- app/src/main/java/com/example/flights/ui/theme/Type.kt
+++ app/src/main/java/com/example/flights/ui/theme/Type.kt
@@ -1,34 +1,113 @@
 package com.example.flights.ui.theme
 
-import androidx.compose.material3.Typography
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.ExperimentalTextApi
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontVariation
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.flights.R
+
+@OptIn(ExperimentalTextApi::class)
+val inter = FontFamily(
+    Font(
+        R.font.inter,
+        weight = FontWeight.Normal,
+        variationSettings = FontVariation.Settings(
+            FontVariation.weight(FontWeight.Normal.weight)
+        )
+    ),
+    Font(
+        R.font.inter,
+        weight = FontWeight.Medium,
+        variationSettings = FontVariation.Settings(
+            FontVariation.weight(FontWeight.Medium.weight)
+        )
+    ),
+    Font(
+        R.font.inter,
+        weight = FontWeight.SemiBold,
+        variationSettings = FontVariation.Settings(
+            FontVariation.weight(FontWeight.SemiBold.weight)
+        )
+    ),
+    Font(
+        R.font.inter,
+        weight = FontWeight.Bold,
+        variationSettings = FontVariation.Settings(
+            FontVariation.weight(FontWeight.Bold.weight)
+        )
+    ),
+)
 
 // Set of Material typography styles to start with
-val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
+fun createFlightsTypography(textColor: Color) = FlightsTypography(
+    textXs = TextStyle(
+        fontFamily = inter,
+        fontSize = 12.sp,
+        lineHeight = 18.sp,
+        color = textColor
+    ),
+    textSm = TextStyle(
+        fontFamily = inter,
+        fontSize = 14.sp,
+        lineHeight = 20.sp,
+        color = textColor
+    ),
+    textMd = TextStyle(
+        fontFamily = inter,
         fontSize = 16.sp,
         lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
+        color = textColor
     ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
+    textLg = TextStyle(
+        fontFamily = inter,
+        fontSize = 18.sp,
+        lineHeight = 28.sp,
+        color = textColor
+    ),
+    textXl = TextStyle(
+        fontFamily = inter,
+        fontSize = 20.sp,
+        lineHeight = 30.sp,
+        color = textColor
+    ),
+    displayXs = TextStyle(
+        fontFamily = inter,
+        fontSize = 24.sp,
+        lineHeight = 32.sp,
+        color = textColor
+    ),
+    displaySm = TextStyle(
+        fontFamily = inter,
+        fontSize = 30.sp,
+        lineHeight = 38.sp,
+        color = textColor
+    ),
+    displayMd = TextStyle(
+        fontFamily = inter,
+        fontSize = 36.sp,
+        lineHeight = 44.sp,
+        color = textColor
+    ),
+    displayLg = TextStyle(
+        fontFamily = inter,
+        fontSize = 48.sp,
+        lineHeight = 60.sp,
+        color = textColor
+    ),
+    displayXl = TextStyle(
+        fontFamily = inter,
+        fontSize = 60.sp,
+        lineHeight = 72.sp,
+        color = textColor
+    ),
+    display2xl = TextStyle(
+        fontFamily = inter,
+        fontSize = 72.sp,
+        lineHeight = 90.sp,
+        color = textColor
     )
-    */
 )
